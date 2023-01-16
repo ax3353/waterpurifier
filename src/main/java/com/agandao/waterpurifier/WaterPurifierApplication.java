402b3e7b7ee95916f6c1150cd6f1848a07718c44
ax3353 <247213579@qq.com>
2023-01-16 16:04:10 +0800

    init

---
+++ src/main/java/com/agandao/waterpurifier/WaterPurifierApplication.java
@@ -0,0 +1,13 @@
+package com.agandao.waterpurifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterPurifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WaterPurifierApplication.class, args);
+    }
+
+}
